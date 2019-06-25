722a4630a7dac758329e9fe948b64b9961264124
radumihai1 <mrmihai809@gmail.com>
2019-06-25 15:55:13 +0300

    design

--- git_new.bat
+++ git_new.bat
@@ -1,15 +1,20 @@
 set /p n=Enter name: 
 echo user entered %n%
+
 set /p c=Enter first commit: 
 echo user entered %c%
+
 set url="https://github.com/radumihai1/%n%.git"
 echo url %url%
+
+
 echo "# %n%" >> README.md
 git init
 git add README.md
 git commit -m "%c%"
 git remote add origin %url%
-
 git push -u origin master
+
+
 set /p return=0:
 echo return %return%
