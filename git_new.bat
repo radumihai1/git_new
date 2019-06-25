0b1da8376b3ea2ae86e2cd0bce8301cd90038590
radumihai1 <mrmihai809@gmail.com>
2019-06-25 15:05:51 +0300

    new git one click

---
+++ git_new.bat
@@ -0,0 +1,14 @@
+set /p n=Enter name: 
+echo user entered %n%
+set /p c=Enter first commit: 
+echo user entered %c%
+set url=https://github.com/radumihai1/%n%.git
+echo url %url%
+echo "# %n%" >> README.md
+git init
+git add README.md
+git commit -m "%c%"
+git remote add origin %url%
+git push -u origin masterd
+set /p return=0:
+echo return %return%
