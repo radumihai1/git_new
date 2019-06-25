cbfaabfad53afecb5cbec089c676f4e211aae314
radumihai1 <mrmihai809@gmail.com>
2019-06-25 15:09:18 +0300

    bugfix error

--- git_new.bat
+++ git_new.bat
@@ -2,7 +2,7 @@ set /p n=Enter name:
 echo user entered %n%
 set /p c=Enter first commit: 
 echo user entered %c%
-set url=https://github.com/radumihai1/%n%.git
+set url="https://github.com/radumihai1/%n%.git"
 echo url %url%
 echo "# %n%" >> README.md
 git init
